4dc75e19042e2f237b90d382fc5625b5d1d691d0
Faeez Shaikh <faeezshaikh@Faeezs-MacBook-Pro.local>
2016-08-28 14:36:48 -0500

    Cleanup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,200 +1,310 @@
-angular.module('starter.controllers', [ 'socialShareModule','firebase'])
+angular.module('starter.controllers', ['socialShareModule', 'firebase'])
 
-.controller('AppCtrl', function($scope, $stateParams, $state, $http, helperService, $ionicModal, localStorageService, awsService, $rootScope, topicMap, shareService,$window,$ionicScrollDelegate) {
+	.controller('AppCtrl', function ($scope, $stateParams, $state, $http, helperService, $ionicModal, localStorageService, awsService, $rootScope, topicMap, shareService, $window, $ionicScrollDelegate) {
 
-	var warned = false;
-	var practiceExam = false;
-	var examTopic = $stateParams.topicId;
+		////////// Declarations ////////////
+		var warned = false;
+		var practiceExam = false;
+		var examTopic = $stateParams.topicId;
+		var tSetting = awsService.getTimerSettings();
+		if (tSetting === 'on') {
+			$scope.showTimer = true;
+		} else {
+			$scope.showTimer = false;
+		}
+		$scope.toggleTimer = function (val) {
+			awsService.setTimerSettings(val);
+		}
 
-	var tSetting = awsService.getTimerSettings();
-	if (tSetting === 'on') {
-		$scope.showTimer = true;
-	} else {
-		$scope.showTimer = false;
-	}
+		$scope.mode = {
+			'value': 'quiz'
+		};
 
-	$scope.toggleTimer = function(val) {
-		awsService.setTimerSettings(val);
-	}
+		$scope.topics = awsService.getTopics();
+		$scope.mockExams = awsService.getMockExams();
 
-	////////// Explanation Feature Starts   /////////
-	$scope.showExplanation = function(expl,reference,topicId) {
-		$scope.explantion = expl;
-		$scope.ref = "'" + reference + "'";
-		$scope.explanationTopic = topicId;
-		$ionicModal.fromTemplateUrl('templates/explanationModal.html', {
-			scope : $scope
-		}).then(function(modal) {
-			$scope.explModal = modal;
-			$scope.explModal.show();
+		$ionicModal.fromTemplateUrl('templates/timeup.html', {
+			scope: $scope
+		}).then(function (modal) {
+			$scope.modal = modal;
 		});
-	}
-	$scope.closeExplanation = function(topicId) {
-		$scope.explModal.hide();
-		console.log('closing');
+		////////// Declarations ////////////
+
+
+		///////// Load Quiz Start ///////
+
+
+		function loadQuiz(examTopic) {
+
+			$scope.questions = [];
+			$scope.timer = 900;
+			$scope.wrong = 0;
+			$scope.score = 0;
+
+			file = topicMap[examTopic];
+
+			if (examTopic == 100 || examTopic == 200 || examTopic == 300) {
+				// If its one of the mock exam
+				practiceExam = true;
+
+				if ($scope.showTimer) {
+					$scope.timer = 2400;  // 40 minutes for practice exam
+				}
+
+				// Show a warning modal first
+				if (warned) {
+					// If warned, show the timer and set appropriate time.
+					$scope.timer = 2400;
+					$scope.showTimer = true;
+
+					if (examTopic == 300 || examTopic == 200) {
+						$scope.timer = 4800;   // 80 minutes for cert exam
+					}
+
+				} else {
+					// Show the modal if not warned.
+					if (examTopic == 200 || examTopic == 300) {
+						$scope.bigExam = 'big'; // the Warning modal differs for practice exam vs certification exam in title
+					}
+
+					$ionicModal.fromTemplateUrl('templates/warningModal.html', {
+						scope: $scope
+					}).then(function (modal) {
+						$scope.warningModal = modal;
+						$scope.warningModal.show();
+					});
+				}
+
+			}
+
+			// once the file source is identifed, call service to get contents of file.
+			$http.get(file).then(function (res) {
+				$scope.quiz = res.data.quiz;
+				$scope.config = helperService.extend({}, $scope.defaultConfig, res.data.config);
+				$scope.questions = $scope.config.shuffleQuestions ? helperService.shuffle(res.data.questions) : res.data.questions;
+				// $scope.questions =  res.data.questions;
+				$scope.totalItems = $scope.questions.length;
+				$scope.totalQues = $scope.questions.length;
+				$scope.itemsPerPage = $scope.config.pageSize;
+				// $scope.itemsPerPage = 1;
+				$scope.currentPage = 1;
+				$scope.mode.value = 'quiz';
+
+				$scope.$watch('currentPage + itemsPerPage', function () {
+					var begin = (($scope.currentPage - 1) * $scope.itemsPerPage), end = begin + $scope.itemsPerPage;
+
+					$scope.filteredQuestions = $scope.questions.slice(begin, end);
+				});
+			});
+		}
+
+		if (examTopic) {
+			loadQuiz(examTopic);
+		}
+		///////// Load Quiz End ///////
+
+
+		//Note: Only those configs are functional which is documented at: http://www.codeproject.com/Articles/860024/Quiz-Application-in-AngularJs
+		// Others are work in progress.
+		$scope.defaultConfig = {
+			'allowBack': true,
+			'allowReview': true,
+			'autoMove': false, // if true, it will move to next question automatically when answered.
+			'duration': 0, // indicates the time in which quiz needs to be completed. post that, quiz will be automatically submitted. 0 means unlimited.
+			'pageSize': 1,
+			'requiredAll': false, // indicates if you must answer all the questions before submitting.
+			'richText': false,
+			'shuffleQuestions': true,
+			'shuffleOptions': true,
+			'showClock': true,
+			'showPager': true,
+			'theme': 'none'
+		}
+
+		$scope.goTo = function (index) {
+			if (index > 0 && index <= $scope.totalItems) {
+				$scope.currentPage = index;
+				$scope.mode.value = 'quiz';
+			}
+			console.log("scrolling");
+			$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop(true);
+		}
+
+		$scope.onSelect = function (question, option) {
+			// if (question.QuestionTypeId == 1) { 
+			question.Options.forEach(function (element, index, array) {
+				if (element.Id != option.Id) {
+					// element.Selected = false;
+					question.Answered = element.Id;
+				}
+			});
+			// }
+			/*
+			 if ($scope.config.autoMove == true && $scope.currentPage < $scope.totalItems)
+			 $scope.currentPage++;*/
+		}
+
+		$scope.onSubmit = function () {
+
+			if (practiceExam) {
+				// For mock exams we show a warning message
+
+				$ionicModal.fromTemplateUrl('templates/submitConfirm.html', {
+					scope: $scope
+				}).then(function (modal) {
+					$scope.submitModal = modal;
+					$scope.submitModal.show();
+				});
+			} else {
+
+				// Otherwise we dont show warning and directly calculate score.
+				calculateAndUpdateScore();
+				
+			}
+			$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop(true);
+		}
+
+
+
+		$scope.isAnswered = function (index) {
+			var answered = 'Not Answered';
+			$scope.questions[index].Options.forEach(function (element, index, array) {
+				if (element.Selected == true) {
+					answered = 'Answered';
+					return false;
+				}
+			});
+			return answered;
+		};
+
+		$scope.isCorrect = function (question) {
+			var result = 'Correct';
+			question.Options.forEach(function (option, index, array) {
+				if (toBool(option.Selected) != option.IsAnswer) {
+					result = 'Wrong';
+					return false;
+				}
+			});
+			return result;
+		};
+
+		function toBool(val) {
+			if (val == 'undefined' || val == null || val == '' || val == 'false' || val == 'False')
+				return false;
+			else if (val == true || val == 'true' || val == 'True')
+				return true;
+			else
+				return 'unidentified';
+		}
+		;
+
+		$scope.timerElapsed = function () {
+			$scope.modal.show();
+		}
+
+		$scope.viewResults = function () {
+			$scope.modal.hide();
+			$scope.onSubmit();
+		};
+
+		$scope.closeResults = function () {
+			$scope.mode.value = 'quiz';
+			$state.go('app.topics');
+		}
+
+		$scope.closeWarning = function () {
+			$scope.warningModal.hide();
+			$state.go('app.topics');
+		}
+
+		$scope.cancelSubmit = function () {
+			$scope.submitModal.hide();
+		}
+
+		$scope.confirmSubmit = function () {
+			$scope.submitModal.hide();
+			calculateAndUpdateScore();
+		}
+
+		$scope.startPracticeExam = function () {
+			$scope.warningModal.hide();
+			warned = true;
+			loadQuiz(examTopic);
+		}
+
+		function calculateAndUpdateScore() {
+
+			$scope.wrong = 0;
+			$scope.score = 0;
+
+			$scope.$broadcast('timer-stop');
+
+			$scope.questions.forEach(function (q, index) {
+				if ($scope.isCorrect(q) == 'Correct') {
+
+				} else {
+					$scope.wrong++;
+				}
+
+			});
+
+			var correct = $scope.totalQues - $scope.wrong;
+			$scope.score = Math.round((Number(correct) / $scope.totalQues) * 100);
+			$scope.verdict = ($scope.score > 65) ? 'Pass' : 'Fail';
+
+			awsService.updateScoreForTopics(examTopic, $scope.score);
+			$scope.mode.value = 'result';
+			drawCirclePercentage();
+		}
+
+		$scope.shareFb = function () {
+			shareService.shareOnFb();
+		}
+		$scope.shareTwitter = function () {
+			shareService.shareOnTwitter();
+		}
+		$scope.shareWhatsapp = function () {
+			shareService.shareOnWhatsapp();
+		}
+		$scope.shareSms = function () {
+			shareService.shareViaSms();
+		}
+		$scope.shareEmail = function () {
+			shareService.shareViaEmail();
+		}
+
+
+		////////// Explanation Feature Starts   /////////
+		$scope.showExplanation = function (expl, reference, topicId) {
+			$scope.explantion = expl;
+			$scope.ref = "'" + reference + "'";
+			$scope.explanationTopic = topicId;
+			$ionicModal.fromTemplateUrl('templates/explanationModal.html', {
+				scope: $scope
+			}).then(function (modal) {
+				$scope.explModal = modal;
+				$scope.explModal.show();
+			});
+		}
+		$scope.closeExplanation = function (topicId) {
+			$scope.explModal.hide();
+			console.log('closing');
 
 			/*  After opening a new window from the explanation modal, 
 		 			when you hit 'Close' the location changes to the chat home screen, 
 		 			so forcing it to go back to the topics results page */
-		$window.location.href = "#/app/topics/" + topicId;
-		
-	}
-	$scope.openLink = function(link) {
-		window.open(link, '_system', 'location=yes'); 
-		return false;
-	}
+			$window.location.href = "#/app/topics/" + topicId;
+
+		}
+		$scope.openLink = function (link) {
+			window.open(link, '_system', 'location=yes');
+			return false;
+		}
 		///////// Explanation Feature Ends  //////////
 
 
-	function loadQuiz(examTopic) {
-		
-
-		$scope.questions = [];
-		$scope.timer = 900;
-		$scope.wrong = 0;
-		$scope.score = 0;
-
-		file = topicMap[examTopic];
-
-		if (examTopic == 100 || examTopic == 200 || examTopic == 300 || examTopic == 400 || examTopic == 500) {
-			// If its one of the mock exam
-			practiceExam = true;
-
-			if ($scope.showTimer) {
-				$scope.timer = 2400;  // 40 minutes for practice exam
-			}
-
-			// Show a warning modal first
-			if (warned) {
-				// If warned, show the timer and set appropriate time.
-				$scope.timer = 2400;
-				$scope.showTimer = true;
-
-				if (examTopic == 300 || examTopic == 200 ) {
-					$scope.timer = 4800;   // 80 minutes for cert exam
-				}
-				if (examTopic == 400) {
-					$scope.timer = 2400;  // 40 minutes for warmup exams
-				}
-
-			} else {
-				// Show the modal if not warned.
-				if (examTopic == 200  || examTopic == 300) {
-					$scope.bigExam = 'big'; // the Warning modal differs for practice exam vs certification exam in title
-				}
-//				if (examTopic == 400) {
-//					$scope.bigExam = 'mini'; // the Warning modal differs for practice exam vs certification exam in title
-//				}
-
-				$ionicModal.fromTemplateUrl('templates/warningModal.html', {
-					scope : $scope
-				}).then(function(modal) {
-					$scope.warningModal = modal;
-					$scope.warningModal.show();
-				});
-			}
-
-		}
-
-		// once the file source is identifed, call service to get contents of file.
-		$http.get(file).then(function(res) {
-			$scope.quiz = res.data.quiz;
-			$scope.config = helperService.extend({}, $scope.defaultConfig, res.data.config);
-			$scope.questions = $scope.config.shuffleQuestions ? helperService.shuffle(res.data.questions) : res.data.questions;
-			// $scope.questions =  res.data.questions;
-			$scope.totalItems = $scope.questions.length;
-			$scope.totalQues = $scope.questions.length;
-			$scope.itemsPerPage = $scope.config.pageSize;
-			// $scope.itemsPerPage = 1;
-			$scope.currentPage = 1;
-			$scope.mode.value = 'quiz';
-
-			$scope.$watch('currentPage + itemsPerPage', function() {
-				var begin = (($scope.currentPage - 1) * $scope.itemsPerPage), end = begin + $scope.itemsPerPage;
-
-				$scope.filteredQuestions = $scope.questions.slice(begin, end);
-			});
-		});
-	}
-
-	if (examTopic) {
-			loadQuiz(examTopic);
-	}
-
-	$scope.mode = {
-		'value' : 'quiz'
-	};
-
-	$scope.topics = awsService.getTopics();
-	$scope.mockExams = awsService.getMockExams();
-
-	$ionicModal.fromTemplateUrl('templates/timeup.html', {
-		scope : $scope
-	}).then(function(modal) {
-		$scope.modal = modal;
-	});
-
-	//Note: Only those configs are functional which is documented at: http://www.codeproject.com/Articles/860024/Quiz-Application-in-AngularJs
-	// Others are work in progress.
-	$scope.defaultConfig = {
-		'allowBack' : true,
-		'allowReview' : true,
-		'autoMove' : false, // if true, it will move to next question automatically when answered.
-		'duration' : 0, // indicates the time in which quiz needs to be completed. post that, quiz will be automatically submitted. 0 means unlimited.
-		'pageSize' : 1,
-		'requiredAll' : false, // indicates if you must answer all the questions before submitting.
-		'richText' : false,
-		'shuffleQuestions' : true,
-		'shuffleOptions' : true,
-		'showClock' : true,
-		'showPager' : true,
-		'theme' : 'none'
-	}
-
-	$scope.goTo = function(index) {
-		if (index > 0 && index <= $scope.totalItems) {
-			$scope.currentPage = index;
-			$scope.mode.value = 'quiz';
-		}
-			console.log("scrolling");
-		$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop(true);
-	}
-
-	$scope.onSelect = function(question, option) {
-		// if (question.QuestionTypeId == 1) { 
-		question.Options.forEach(function(element, index, array) {
-			if (element.Id != option.Id) {
-				// element.Selected = false;
-				question.Answered = element.Id;
-			}
-		});
-		// }
-		/*
-		 if ($scope.config.autoMove == true && $scope.currentPage < $scope.totalItems)
-		 $scope.currentPage++;*/
-	}
-
-	$scope.onSubmit = function() {
-
-		if (practiceExam) {
-			// For mock exams we show a warning message
-
-			$ionicModal.fromTemplateUrl('templates/submitConfirm.html', {
-				scope : $scope
-			}).then(function(modal) {
-				$scope.submitModal = modal;
-				$scope.submitModal.show();
-			});
-		} else {
-
-			// Otherwise we dont show warning and directly calculate score.
-			calculateAndUpdateScore();
-			drawCirclePercentage();
-		}
-		$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop(true);
-	}
-
-	function drawCirclePercentage() {
+		////// Draw Circular percentage bar //////
+		function drawCirclePercentage() {
 			$('#circle').radialIndicator({
 				barColor: {
 					0: '#ff1a33',
@@ -210,113 +320,7 @@ angular.module('starter.controllers', [ 'socialShareModule','firebase'])
 
 			var radialObj = $('#circle').data('radialIndicator');
 			radialObj.animate(60);
-	}
+		}
+		////// Draw Circular percentage bar //////
 
-	$scope.isAnswered = function(index) {
-		var answered = 'Not Answered';
-		$scope.questions[index].Options.forEach(function(element, index, array) {
-			if (element.Selected == true) {
-				answered = 'Answered';
-				return false;
-			}
-		});
-		return answered;
-	};
-
-	$scope.isCorrect = function(question) {
-		var result = 'Correct';
-		question.Options.forEach(function(option, index, array) {
-			if (toBool(option.Selected) != option.IsAnswer) {
-				result = 'Wrong';
-				return false;
-			}
-		});
-		return result;
-	};
-
-	function toBool(val) {
-		if (val == 'undefined' || val == null || val == '' || val == 'false' || val == 'False')
-			return false;
-		else if (val == true || val == 'true' || val == 'True')
-			return true;
-		else
-			return 'unidentified';
-	}
-	;
-
-	$scope.timerElapsed = function() {
-		$scope.modal.show();
-	}
-
-	$scope.viewResults = function() {
-		$scope.modal.hide();
-		$scope.onSubmit();
-	};
-
-	$scope.closeResults = function() {
-		$scope.mode.value = 'quiz';
-		$state.go('app.topics');
-	}
-
-	$scope.closeWarning = function() {
-		$scope.warningModal.hide();
-		$state.go('app.topics');
-	}
-
-	$scope.cancelSubmit = function() {
-		$scope.submitModal.hide();
-	}
-
-	$scope.confirmSubmit = function() {
-		$scope.submitModal.hide();
-		calculateAndUpdateScore();
-	}
-
-	$scope.startPracticeExam = function() {
-		$scope.warningModal.hide();
-		warned = true;
-		loadQuiz(examTopic);
-	}
-
-	function calculateAndUpdateScore() {
-
-		$scope.wrong = 0;
-		$scope.score = 0;
-
-		$scope.$broadcast('timer-stop');
-
-		$scope.questions.forEach(function(q, index) {
-			if ($scope.isCorrect(q) == 'Correct') {
-
-			} else {
-				$scope.wrong++;
-			}
-
-		});
-
-		var correct = $scope.totalQues - $scope.wrong;
-		$scope.score = Math.round((Number(correct) / $scope.totalQues) * 100);
-		$scope.verdict = ($scope.score > 65) ? 'Pass' : 'Fail';
-
-		awsService.updateScoreForTopics(examTopic, $scope.score);
-		$scope.mode.value = 'result';
-	}
-
-	$scope.shareFb = function() {
-		shareService.shareOnFb();
-	}
-	$scope.shareTwitter = function() {
-		shareService.shareOnTwitter();
-	}
-	$scope.shareWhatsapp = function() {
-		shareService.shareOnWhatsapp();
-	}
-	$scope.shareSms = function() {
-		shareService.shareViaSms();
-	}
-	$scope.shareEmail = function() {
-		shareService.shareViaEmail();
-	}
-
-
-});
+	});
